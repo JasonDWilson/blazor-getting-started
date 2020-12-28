8ad206b0fcf799382c3fa0ea0948e5a82345db54
Jason Wilson <jason.wilson@flashparking.com>
2020-12-28 14:31:35 -0600

    Added Job Category data service

---
+++ BethanysPieShowHRM.App/Services/JobCategoryDataService.cs
@@ -0,0 +1,33 @@
+﻿using BethanysPieShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BethanysPieShowHRM.App.Services
+{
+    public class JobCategoryDataService : IJobCategoryDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public JobCategoryDataService(HttpClient client) => _httpClient =
+            client ?? throw new ArgumentNullException(nameof(client));
+
+        public async Task<IEnumerable<JobCategory>> GetJobCategoriesAsync()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<JobCategory>>(
+                await _httpClient.GetStreamAsync(string.Empty),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+
+        public async Task<JobCategory> GetJobCategoryAsync(int JobCategoryId)
+        {
+            return await JsonSerializer.DeserializeAsync<JobCategory>(
+                await _httpClient.GetStreamAsync($"{JobCategoryId}"),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
