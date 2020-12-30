2aa39a0bb62b427a16b5606a8ed20ea608ae82c5
Jason Wilson <jason.wilson@flashparking.com>
2020-12-30 13:46:19 -0600

    set map componet to be lazy loaded

--- BethanysPieShopHRM.Shared/Marker.cs
+++ BethanysPieShopHRM.Shared/Marker.cs
@@ -1,13 +1,13 @@
-﻿namespace BethanysPieShopHRM.ComponentsLibrary.Map
+﻿namespace BethanysPieShopHRM.Shared
 {
     public class Marker
     {
         public string Description { get; set; }
 
+        public bool ShowPopup { get; set; }
+
         public double X { get; set; }
 
         public double Y { get; set; }
-
-        public bool ShowPopup { get; set; }
     }
 }
